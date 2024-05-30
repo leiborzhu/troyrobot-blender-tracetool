103c136e406798fd6289120ca42b6fa8e23f94d1
zhuhe <zhuhe@zhejianglab.com>
2024-05-30 16:30:04 +0800

    0.8.3tmp

--- __init__.py
+++ __init__.py
@@ -1,24 +1,24 @@
 bl_info = {
     "name": "track_tool",
     "author": "zhuhe",
-    "version": (0, 8, 3),
-    "blender": (3, 6, 0),
+    "version": (0, 8, 4),
+    "blender": (3, 6, 8),
     "location": "View3D > Sidebar >Trace manage",
     "description": "轨迹管理工具",
     "warning": "",
     "doc_url": "",
     "category": "Object",
 }
-# 0.8.2 -> 0.8.3更新
-# 去除首尾链接
-# mac临时文件夹地址优化
-# todo: 法线换算
+# 0.8.4更新
+# 轨迹组件待优化
+# 导出待优化
 
 import bpy
 import os
 import sys
 import json
 import math
+import numpy as np
 
 from bpy.props import (
         IntProperty,
@@ -79,7 +79,7 @@ def json2obj(list_data, tmp_path:str, name:str):
 
     for i in range(num - 1):
         line_lines.append(f"l {i+1} {i+2}\n")
-    line_lines.append(f"l {num} 1\n") # 当前默认轨迹首尾相连
+    # line_lines.append(f"l {num} 1\n") # 当前默认轨迹不首尾相连
 
     # 写入obj文件
     all_lines = point_lines + normal_lines + line_lines
@@ -191,17 +191,20 @@ class Track_input(bpy.types.Operator):
 
 # ------------导出轨迹类&运行函数-----------
 def point_list_write(traph, label, tmp_data):
-    scale = 1 # 单位强制设置为毫米，此处scale为1000
+    scale = 1000 # 单位强制设置为毫米，此处scale为1000
     point_list = []
     pos_list = []
     nor_list = []
     num = len(bpy.data.meshes[traph.track_name + '_' + label].vertices) # example_name
+    
     for i in range(num):
         obj = bpy.data.objects[traph.track_name + '_' + label + '_' + str(i)] # example_name_0
 
         pos_list.append(obj.location)
         # 弧度制 默认XYZ
-        nor_list.append([obj.rotation_euler[0], obj.rotation_euler[1], obj.rotation_euler[2]])
+        # todo 欧拉旋转数据转换为法线
+        nor_out = euler_to_normal([obj.rotation_euler[0], obj.rotation_euler[1], obj.rotation_euler[2]])
+        nor_list.append(nor_out)
 
 
 
@@ -231,6 +234,27 @@ def build_normal_obj(traph, pos, nor, index, track_name):
     obj_tmp.rotation_euler[0] = nor[0]
     obj_tmp.rotation_euler[1] = nor[1]
     obj_tmp.rotation_euler[2] = nor[2]
+    # 增加法线末端点
+    bpy.ops.object.empty_add(type='SINGLE_ARROW', align='WORLD', location=(pos[0]/1000 + nor[0]*0.01, pos[1]/1000 + nor[1]*0.01, pos[2]/1000 + nor[2]*0.01), scale=(scale, scale, scale))
+    nor_tmp = bpy.context.object
+    nor_tmp.name = track_name + '_' + str(index) + '_norend'
+    nor_tmp.scale[0] = scale*3
+    nor_tmp.scale[1] = scale*3
+    nor_tmp.scale[2] = scale
+
+    # 增加约束器
+    cons_rotation = obj_tmp.constraints.new('TRACK_TO')
+    cons_rotation.name = 'normal'
+    cons_rotation.target = bpy.data.objects[track_name + '_' + str(index) + '_norend']
+    cons_rotation.track_axis = 'TRACK_Z'
+    cons_rotation.up_axis = 'UP_Y'
+    cons_rotation.target_space = 'WORLD'
+    cons_rotation.owner_space = 'WORLD'
+
+    # bpy.ops.constraint.apply(constraint=cons_rotation, owner='OBJECT')
+    nor_tmp.hide_viewport = True
+    # bpy.data.objects.remove(bpy.data.objects[track_name + '_' + str(index) + '_norend'])
+
     """
     # 添加驱动器() # 该方案暂时搁置
     dri_pos_x = obj_tmp.driver_add('location', 0).driver
@@ -278,6 +302,52 @@ def build_normal_obj(traph, pos, nor, index, track_name):
     dri_config_location(dri_pos_z, config_pos_z)
     """
 
+def normal_to_euler(nor):
+
+    dx = 0
+    dy = 0
+    dz = 1
+
+    dx_, dy_, dz_ = nor[0], nor[1], nor[2]
+
+    # 计算点积
+    dot_product = dx * dx_ + dy * dy_ + dz * dz_
+    
+    # 计算夹角
+    angle = math.acos(dot_product)
+    
+    # 计算rx
+    rx = math.atan2(dz_, math.sqrt(dx_**2 + dy_**2))
+    
+    # 计算ry
+    ry = math.atan2(dy_, math.sqrt(dx_**2 + dz_**2))
+    
+    # 计算rz
+    rz = math.atan2(dx_, math.sqrt(dy_**2 + dz_**2))
+
+    return [rx, ry, rz]
+
+def euler_to_normal(euler):
+    x_euler = euler[0]
+    y_euler = euler[1]
+    z_euler = euler[2]
+    # 初始向量 [0, 0, 1]
+    
+    # 计算旋转矩阵
+    Rx = np.array([[1, 0, 0],
+               [0, np.cos(x_euler), -np.sin(x_euler)],
+               [0, np.sin(x_euler), np.cos(x_euler)]])
+
+    # 绕y轴旋转的矩阵
+    Ry = np.array([[np.cos(y_euler), 0, np.sin(y_euler)],
+               [0, 1, 0],
+               [-np.sin(y_euler), 0, np.cos(y_euler)]])
+
+    Rz = np.array([[np.cos(z_euler), -np.sin(z_euler), 0],
+                [np.sin(z_euler), np.cos(z_euler), 0],
+                [0, 0, 1]])
+
+    return list(np.dot(np.dot(np.dot([0, 0, 1], Rx), Ry), Rz))
 
 def normal_sim(tmp_path, traph):
     # 导入法线数据
@@ -299,13 +369,14 @@ def normal_sim(tmp_path, traph):
 
     for i, vertice in enumerate(bpy.data.meshes[traph.track_name + '_surface'].vertices):
         pos_cur = vertice.co
-        nor_cur = nor_surface[i]
-        build_normal_obj(traph, pos_cur, nor_cur, i, traph.track_name + '_surface')
+        # 换算
+        euler_cur = nor_surface[i]
+        build_normal_obj(traph, pos_cur, euler_cur, i, traph.track_name + '_surface')
         
     for i, vertice in enumerate(bpy.data.meshes[traph.track_name + '_edge'].vertices):
         pos_cur = vertice.co
-        nor_cur = nor_edge[i]
-        build_normal_obj(traph, pos_cur, nor_cur, i, traph.track_name + '_edge')
+        euler_cur = nor_edge[i]
+        build_normal_obj(traph, pos_cur, euler_cur, i, traph.track_name + '_edge')
     
     
     
@@ -386,6 +457,10 @@ def track_update(traph, tmp_path):
     init_trans(bpy.context.object)
     bpy.ops.wm.obj_import(filepath=os.path.join(tmp_path, f'{traph.track_name}_edge.obj'), directory=tmp_path, global_scale=1)
     init_trans(bpy.context.object)
+
+    # 将轨迹本身设为不可选， 避免操作点时误选
+    bpy.data.objects[traph.track_name + '_surface'].hide_select = True
+    bpy.data.objects[traph.track_name + '_edge'].hide_select = True
 
     
 
